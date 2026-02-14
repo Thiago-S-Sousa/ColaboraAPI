b0c5cb0b5c20feb6ab5af71b0276295a78afb2fb
Thiago-S-Sousa <Thiago_S_Sousa@outlook.com>
2026-02-14 10:03:59 -0300

    feat: pasta Models e classes FuncionarioModel e ServiceResponse adicionadas

---
+++ Colabora/Models/FuncionarioModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Colabora.Models
+{
+    public class FuncionarioModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+        public string Departamento { get; set; }
+        public bool Ativo { get; set; }
+        public string Turno { get; set; }
+        public DateTime DataDeCriacao { get; set; } = DateTime.Now.ToLocalTime();
+        public DateTime DataDeAlteracao { get; set; } = DateTime.Now.ToLocalTime();
+
+    }
+}
