60dbc11689e34df965239a870e86ce6bc8d9d2b5
Thiago-S-Sousa <Thiago_S_Sousa@outlook.com>
2026-02-14 10:17:11 -0300

    refactor: enums DepartamentoEnum e TurnoEnum adicionados nas suas respectivas propriedades

--- Colabora/Models/FuncionarioModel.cs
+++ Colabora/Models/FuncionarioModel.cs
@@ -1,13 +1,15 @@
-﻿namespace Colabora.Models
+﻿using Colabora.Enums;
+
+namespace Colabora.Models
 {
     public class FuncionarioModel
     {
         public int Id { get; set; }
         public string Nome { get; set; }
         public string SobreNome { get; set; }
-        public string Departamento { get; set; }
+        public DepartamentoEnum Departamento { get; set; }
         public bool Ativo { get; set; }
-        public string Turno { get; set; }
+        public TurnoEnum Turno { get; set; }
         public DateTime DataDeCriacao { get; set; } = DateTime.Now.ToLocalTime();
         public DateTime DataDeAlteracao { get; set; } = DateTime.Now.ToLocalTime();
 
