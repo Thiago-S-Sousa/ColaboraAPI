db159ba48f78d15949f8e663e34f52fc290fa221
Thiago-S-Sousa <Thiago_S_Sousa@outlook.com>
2026-02-16 10:45:22 -0300

    feat: pasta das Interfaces e IFuncionarioInterface adicionadas

---
+++ Colabora/Interfaces/IFuncionarioInterface.cs
@@ -0,0 +1,19 @@
+﻿using Colabora.Models;
+
+namespace Colabora.Interfaces
+{
+    public interface IFuncionarioInterface
+    {
+        Task<ServiceResponse<List<FuncionarioModel>>> GetFuncionarios();
+
+        Task<ServiceResponse<List<FuncionarioModel>>> CreateFuncionario(FuncionarioModel novoFuncionario);
+
+        Task<ServiceResponse<FuncionarioModel>> GetFuncionarioById(int id);
+
+        Task<ServiceResponse<List<FuncionarioModel>>> UpdateFuncionario(FuncionarioModel editadoFuncionario);
+
+        Task<ServiceResponse<List<FuncionarioModel>>> DeleteFuncionario(int id);
+
+        Task<ServiceResponse<List<FuncionarioModel>>> InactiveFuncionario(int id);
+    }
+}
